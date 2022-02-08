9ef8ff2cd57f3640ea8d6b2dbd9fba8b81390002
celebrater <tkgm45nbchk@gmail.com>
2022-02-08 21:52:33 +0900

    fixed bugs

--- main.js
+++ main.js
@@ -1,20 +1,18 @@
 "use strict";
 
-// const button = document.getElementById("start");
 const kuji = () => {
     const answer = document.getElementById("answer");
     const input = document.querySelector('[name="num"]').value;
-    // console.log(num);
-    // console.log(parseInt(num));
     let str = "2以上の整数を入力してください。";
     const num = parseInt(input);
     console.log(typeof num);
     console.log(num);
     if(num > 1){
-        console.log("num is true");
-        answer.innerText = Math.ceil(Math.random() * num);
+        // console.log("num is true");
+        str = "<span class='num'>" + Math.ceil(Math.random() * num) + "</span>";
     } else {
-        console.log("num is false");
+        // console.log("num is false");
         // answer.innerText(str);
     }
+    answer.innerHTML = str;
 }
